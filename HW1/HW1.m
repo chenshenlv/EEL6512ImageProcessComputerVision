cc2dc79e53f56c246bb0d7c1e3cd0fa35d6a8fcb
chenshenlv <36205358+chenshenlv@users.noreply.github.com>
2018-08-31 02:49:21 -0400

    avg to repmat

--- HW1/HW1.m
+++ HW1/HW1.m
@@ -1,18 +1,18 @@
 %display odd channel
-storedStructure_odd = load('odd_channel.mat');
+storedStructure_odd = load('odd_rows.mat');
 imageArray_odd = storedStructure_odd.odd_channel;
 figure(1);
 imshow(imageArray_odd, []);
 
 %display even channel
-storedStructure_even = load('even_corrupted_channel.mat');
+storedStructure_even = load('even_rows.mat');
 imageArray_even = storedStructure_even.even_corrupted_channel;
 figure(2);
 imshow(imageArray_even, []);
 
 %correct the even channel
 avg=sum(storedStructure_odd.odd_channel,2)/size(storedStructure_odd.odd_channel,2);
-storedStructure_even.even_correct_channel=storedStructure_even.even_corrupted_channel(:,1:end)+avg;
+storedStructure_even.even_correct_channel=storedStructure_even.even_corrupted_channel+repmat(avg,1,560);
 figure(3);
 imshow(imageArray_even, []);
 
@@ -24,8 +24,9 @@ imshow(fullimage, []);
 
 % A = [1,2,3;4,5,6;7,8,9;19,20,21]
 % B = [10,11,12;13,14,15;16,17,18;22,23,24]
-% % avg=sum(A,2)/size(A,2)
-% % A=A(:,1:end)+avg
+% avg=sum(A,2)/size(A,2)
+% avg=repmat(avg,1,3)
+% A=A+avg
 %  C = B([1;1]*(1:size(B,1)),:)
 %  C(1:2:end,:) = A
 %  AB = [A,B]';
